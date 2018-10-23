9752b21feea3d0915de1e13ef07c39b449c77ce8
tagirmi <tagirmi@gmail.com>
2018-10-23 20:58:29 +0300

    добавил класс для сбора статистики

---
+++ bulk_stats.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+#include "bulk.h"
+
+namespace hw10 {
+
+class BulkStats
+{
+public:
+  explicit BulkStats(const std::string& name);
+  BulkStats(const BulkStats&) = default;
+  void takeCountOf(const hw7::Bulk&);
+
+  std::string name() const;
+  size_t bulkCount() const;
+  size_t totalCommandCount() const;
+private:
+  std::string m_name;
+  size_t m_bulkCount;
+  size_t m_totalCommandCount;
+};
+
+} // hw10
+
+std::ostream& operator<<(std::ostream&, const hw10::BulkStats&);
