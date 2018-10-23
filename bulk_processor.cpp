6f9ad1e3e4a7ebbd8401450896082ad9a8902fa5
tagirmi <tagirmi@gmail.com>
2018-10-23 23:14:35 +0300

    вывод в консоль в потоке

--- bulk_processor.cpp
+++ bulk_processor.cpp
@@ -2,11 +2,32 @@
 
 #include "bulk_processor.h"
 
+hw10::BulkProcessor::BulkProcessor()
+  : m_promise{}
+  , m_future{m_promise.get_future()}
+  , m_thread{}
+  , m_stats{"log"}
+{
+  auto worker = [](std::future<hw7::Bulk>& future) {
+    while(true) {
+      //TODO проверка на выход
+      //TODO не работает
+      future.wait();
+      std::cout << future.get() << std::endl;
+    }
+  };
+  m_thread = std::thread(worker, std::ref(m_future));
+}
+
+hw10::BulkProcessor::~BulkProcessor()
+{
+  if (m_thread.joinable())
+    m_thread.join();
+}
+
 void hw10::BulkProcessor::update(const hw7::BulkTime&, const hw7::Bulk& bulk)
 {
-  //TODO log thread
-  std::cout << bulk << std::endl;
-
+  m_promise.set_value(bulk);
   m_stats.takeCountOf(bulk);
 }
 
--- bulk_processor.h
+++ bulk_processor.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <future>
+#include <thread>
+
 #include "bulk.h"
 #include "bulk_stats.h"
 
@@ -8,14 +11,17 @@ namespace hw10 {
 class BulkProcessor : public hw7::BulkObserver
 {
 public:
-  BulkProcessor() = default;
-  ~BulkProcessor() = default;
+  BulkProcessor();
+  ~BulkProcessor();
 
   void update(const hw7::BulkTime&, const hw7::Bulk&) override;
 
   BulkStats stats() const;
 
 private:
+  std::promise<hw7::Bulk> m_promise;
+  std::future<hw7::Bulk> m_future;
+  std::thread m_thread;
   BulkStats m_stats{"log"};
 };
 
